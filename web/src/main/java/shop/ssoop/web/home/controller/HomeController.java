0163908a3ad96f75cfe7711a49d723f2dce312a2
soobinyoon <ssoop.yoon@gmail.com>
2022-04-10 15:46:23 +0900

    Initial commit

---
+++ web/src/main/java/shop/ssoop/web/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package shop.ssoop.web.home.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Hello World!!");
+    }
+}
